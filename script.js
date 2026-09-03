e57295d2893484c0bca1159040ad9447e0e9217f
CCampe <ccampe@lakelandcollege.edu>
2026-09-03 00:02:22 +0000

    Javascript Introduction Project

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("My JavaScript file is connected.");
+console.log("My name is Christopher Campe");
+console.log("I am taking CIS-156: Intro to Programming");
+console.log("I am taking this class to learn how to code");
+console.log("I want like to learn how to properly make a website");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+console.table({ name: "Christopher Campe", course: "CIS 156: Intro to Programming", reason: "I want to learn how to code"});
